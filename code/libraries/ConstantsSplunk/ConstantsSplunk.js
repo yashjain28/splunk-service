dc3f4c8e7a796788b0721bad50f106ba51fc37d3
Yash Jain <yashkjain1994@gmail.com>
2018-04-19 18:09:43 -0500

    Removing unwanted files

--- code/libraries/ConstantsSplunk/ConstantsSplunk.js
+++ code/libraries/ConstantsSplunk/ConstantsSplunk.js
@@ -1,5 +1,5 @@
 var PLATFORM_URL = "https://staging.clearblade.com";
 
-var SPLUNK_KEY= "Splunk 261cdb18-4b63-46c0-8c60-8d18a3d42222"; // Usually of the format "Splunk <HEC-TOKEN>" 
-var SPLUNK_HOSTNAME ="prd-p-jcxp2l4lk9pj.cloud.splunk.com"; // Usually of the format "input-<CLOUD_HOSTNAME>" this example is for getting data from splunk self-service cloud 
+var SPLUNK_KEY= ""; // Usually of the format "Splunk <HEC-TOKEN>" 
+var SPLUNK_HOSTNAME =""; // Usually of the format "input-<CLOUD_HOSTNAME>" this example is for getting data from splunk self-service cloud 
 var PORTNO = "8088";
