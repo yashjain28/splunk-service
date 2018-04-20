2762d3beabf46723821a5153035a222e022c7a82
Yash Jain <yashkjain1994@gmail.com>
2018-04-19 19:09:03 -0500

    Updated portal to send data to right topic

--- code/libraries/ConstantsSplunk/ConstantsSplunk.js
+++ code/libraries/ConstantsSplunk/ConstantsSplunk.js
@@ -1,4 +1,4 @@
-var PLATFORM_URL = "https://staging.clearblade.com";
+var PLATFORM_URL = "";  // "https://platform.clearblade.com";
 
 var SPLUNK_KEY= ""; // Usually of the format "Splunk <HEC-TOKEN>" 
 var SPLUNK_HOSTNAME =""; // <CLOUD_HOSTNAME> this is for getting data from splunk self-service cloud, it is a little different when using Managed Cloud or Splunk Enterprise 
